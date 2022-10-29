930b301adc14921105b3e83f8062b65ce9229c2d
diego-tavares <cdiegostavares@gmail.com>
2022-10-29 17:00:18 -0300

    first commit Hello World

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edite <code>src/App.js</code> e salve para recerregar.
+          Hello World.
         </p>
         <a
           className="App-link"
