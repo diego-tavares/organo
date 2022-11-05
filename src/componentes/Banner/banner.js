6f495d37872226517649f772f80d7bc22447c9e8
diego-tavares <cdiegostavares@gmail.com>
2022-11-05 20:22:01 -0300

    aula 01-04

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    return (
+        <header className='banner'>
+                <img src="/imagens/banner.png" alt="Banner principal da página Organo"/>
+        </header>
+    )
+    
+}
+
+export default Banner
