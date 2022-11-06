2acf4718de0f1f5bf9f7cd96e733dfceb896f0e5
diego-tavares <cdiegostavares@gmail.com>
2022-11-06 17:49:04 -0300

    aula 01-06

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder='Digite seu nome'/>
+      <CampoTexto label="Cargo" placeholder='Digite seu cargo'/>
+      <CampoTexto label="Imagem" placeholder='Digite o endereço da imagem'/>
     </div>
   );
 }
