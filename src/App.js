86055a4c7afb589e223128ffe9c6381403db58cb
diego-tavares <cdiegostavares@gmail.com>
2022-11-06 17:20:02 -0300

    aula 01-06

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from './componentes/Banner/banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto />
     </div>
   );
 }
