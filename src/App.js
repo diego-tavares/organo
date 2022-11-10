1b199070b6584041b1bcd43669b2e3482324428c
diego-tavares <cdiegostavares@gmail.com>
2022-11-09 22:17:32 -0300

    aula 01-08

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/formulario/formulario';
+
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder='Digite seu nome'/>
-      <CampoTexto label="Cargo" placeholder='Digite seu cargo'/>
-      <CampoTexto label="Imagem" placeholder='Digite o endereço da imagem'/>
+      <Formulario />
     </div>
   );
 }
---
+++ src/componentes/formulario/formulario.js
@@ -0,0 +1,18 @@
+import CampoTexto from '../CampoTexto/CampoTexto'
+
+
+const Formulario = () => {
+    return (
+        <section>
+            <form>
+                 <CampoTexto label="Nome" placeholder='Digite seu nome'/>
+                 <CampoTexto label="Cargo" placeholder='Digite seu cargo'/>
+                 <CampoTexto label="Imagem" placeholder='Digite o endereço da imagem'/>
+            </form>
+        </section>
+    )
+    
+
+}
+
+export default Formulario
