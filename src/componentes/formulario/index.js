60772052606ae800185e2d0b9c01dcc28a680369
diego-tavares <cdiegostavares@gmail.com>
2022-11-23 21:11:54 -0300

    aula 01-08

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,9 +1,9 @@
 import CampoTexto from '../CampoTexto'
-
+import './formulario.css'
 
 const Formulario = () => {
     return (
-        <section>
+        <section className="formulario">
             <form>
                 <h2>Preencha os dados para criar  o card do colaborador</h2>
                  <CampoTexto label="Nome" placeholder='Digite seu nome'/>
