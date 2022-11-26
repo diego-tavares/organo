33023ec6c75a36d55bb48739bc85850c4c9c81e1
diego-tavares <cdiegostavares@gmail.com>
2022-11-26 19:37:01 -0300

    aula 01-08

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -5,7 +5,7 @@ const Formulario = () => {
     return (
         <section className="formulario">
             <form>
-                <h2>Preencha os dados para criar  o card do colaborador</h2>
+                <h2>Preencha os dados abaixo para criar  o card do colaborador</h2>
                  <CampoTexto label="Nome" placeholder='Digite seu nome'/>
                  <CampoTexto label="Cargo" placeholder='Digite seu cargo'/>
                  <CampoTexto label="Imagem" placeholder='Digite o endereço da imagem'/>
